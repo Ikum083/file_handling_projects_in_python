c27cf84f90e52d04eddc5bafdd6ce844f12ac830
USER <eugenemabansag8@gmail.com>
2026-04-28 11:39:06 +0800

    Setup odd even seperator py file with pseudocode

---
+++ odd_even_seperator/odd_even_seperator_program.py
@@ -0,0 +1,9 @@
+# odd even seperator
+
+# setup main class
+
+# ask user to input numbers
+
+# put all the inputs in a text file 
+
+# program to read all the numbers in the text file and seperate them to an odd or even text files
