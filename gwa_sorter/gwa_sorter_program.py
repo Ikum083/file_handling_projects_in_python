923d412a6e2f3faa10577264a136dbd9bb920adb
USER <eugenemabansag8@gmail.com>
2026-04-28 12:39:51 +0800

    Added json file to be used and method to add names and gwa

--- gwa_sorter/gwa_sorter_program.py
+++ gwa_sorter/gwa_sorter_program.py
@@ -1,5 +1,13 @@
 # import custom tkinter
 import customtkinter as ctk
+import json
+
+# json file to be used
+try:
+    with open("gwa.json", "r") as file:
+        gwa_dict = json.load(file)
+except json.JSONDecodeError:
+    gwa_dict = {}
 
 # setup main class
 class MainApp(ctk.CTk):
@@ -14,9 +22,6 @@ class MainApp(ctk.CTk):
         self.grid_columnconfigure(0, weight = 1)
         self.grid_rowconfigure((0, 1), weight = 1)
 
-# dictionary to be used
-        self.gwa_dict = {}
-
 # button to open a window to ask for user input
         self.button_sizes_main_menu_x = 480
         self.button_sizes_main_menu_y = 80
@@ -26,7 +31,12 @@ class MainApp(ctk.CTk):
         
         self.highest_gwa = ctk.CTkButton(self, text = "Highest GWA", width = self.button_sizes_main_menu_x, height = self.button_sizes_main_menu_y, font = ("Arial", 40))
         self.highest_gwa.grid(row = 1, column = 0)
+
 ## ask for user input to put 20 gwa with their names
+    def user_input_gwa(self, gwa_dict):
+        input_gwa_window = ctk.CTkToplevel(self)
+
+        enter_gwa = ctk.CTkEntry()
 
 # button to show the name(s) with the highest gwa
 if __name__ == "__main__":
