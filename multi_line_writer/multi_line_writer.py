e8cdd2f780f621b0ce9954123a757e823b5e4135
USER <eugenemabansag8@gmail.com>
2026-04-29 08:21:49 +0800

    Fixed file handling, added condition to stop while loop when user ask for it to stop

--- multi_line_writer/multi_line_writer.py
+++ multi_line_writer/multi_line_writer.py
@@ -1,20 +1,21 @@
 # multi line writer in python
-# boolean to control the while loop
-asking_user = True
-
 # while loop that will iterate until the user says to stop
 class LineWriter():
     def __init__(self):
-        while asking_user:
+        # boolean to control the while loop
+        self.asking_user = True
+        while self.asking_user:
             ## ask user for their input 
             self.user_input = str(input("Enter line: "))
             ## put the input into a text file named "mylife.txt"
-            self.WriterFile = open("mylife.txt", "w")
+            self.WriterFile = open("multi_line_writer/mylife.txt", "a")
             self.WriterFile.write(f"{self.user_input}\n")
-            self.WriterFile.close()
             ## ask user if to continue or stop
             self.stop_or_continue_loop = str(input("Are there more lines y/n? "))
+            if self.stop_or_continue_loop.lower() == 'n':
+                self.asking_user = False
 
+        self.WriterFile.close()
 if __name__ == "__main__":
     LineWriter()
 
