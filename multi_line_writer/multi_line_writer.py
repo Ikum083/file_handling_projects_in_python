2fbcfd4f3aa9e647c59c9bdacafce37e95ddf669
USER <eugenemabansag8@gmail.com>
2026-04-29 08:15:04 +0800

    Added file handling to add the user input into the text file';

--- multi_line_writer/multi_line_writer.py
+++ multi_line_writer/multi_line_writer.py
@@ -9,8 +9,15 @@ class LineWriter():
             ## ask user for their input 
             self.user_input = str(input("Enter line: "))
             ## put the input into a text file named "mylife.txt"
+            self.WriterFile = open("mylife.txt", "w")
+            self.WriterFile.write(f"{self.user_input}\n")
+            self.WriterFile.close()
             ## ask user if to continue or stop
             self.stop_or_continue_loop = str(input("Are there more lines y/n? "))
+
+if __name__ == "__main__":
+    LineWriter()
+
 
 
 
