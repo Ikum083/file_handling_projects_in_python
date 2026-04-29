1e14fd011247c1b7a2c13b77e0187f079a59e468
USER <eugenemabansag8@gmail.com>
2026-04-29 08:06:51 +0800

    Added text file and pseudocode

---
+++ multi_line_writer/multi_line_writer.py
@@ -0,0 +1,10 @@
+# multi line writer in python
+
+# boolean to control the while loop
+
+# while loop that will iterate until the user says to stop
+
+## ask user for their input 
+
+## put the input into a text file named "mylife.txt"
+
