ed43d60f7a24556c3311a33d600b7c19b097758c
USER <eugenemabansag8@gmail.com>
2026-04-29 08:33:13 +0800

    Added for loop to seperate even and odd numbers

--- sqaure_cube_integers/square_cube_integer.py
+++ sqaure_cube_integers/square_cube_integer.py
@@ -5,10 +5,18 @@ class SquareCube():
     def __init__(self):
 ### list to contain every number in the text file
         self.numbers = []
+        self.even_numbers = []
+        self.odd_numbers = []
 ### open file iterating every line on the file
         with open("square_cube_integers/integers.txt", "r") as integers:
             for line in integers:
                 self.numbers.append(line)
 ### seperate every number in the file if they're even or odd
+        for i in self.numbers:
+            if i % 2 == 0:
+                self.even_numbers.append(int(i))
+            else:
+                self.odd_numbers.append(int(i))
 ### square every even number and cube every odd number
+        
 ### input the perspective files into their rightful text file
