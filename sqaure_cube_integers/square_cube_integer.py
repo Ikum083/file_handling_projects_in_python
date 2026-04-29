78abfd986ad6e5205f0302066a386edb66f67f58
USER <eugenemabansag8@gmail.com>
2026-04-29 08:28:19 +0800

    Added more text files to be used and pseudocode

---
+++ sqaure_cube_integers/square_cube_integer.py
@@ -0,0 +1,8 @@
+# square all even integers and cube all odd integers
+
+# setup main class
+## setup main method
+### open file iterating every line on the file
+### seperate every number in the file if they're even or odd
+### square every even number and cube every odd number
+### input the perspective files into their rightful text file
