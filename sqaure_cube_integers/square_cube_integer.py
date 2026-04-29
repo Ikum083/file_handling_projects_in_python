c58d3d9fecd1eb0dc8b7879ec481a95a0e6734ec
USER <eugenemabansag8@gmail.com>
2026-04-29 08:31:07 +0800

    Added file handling to open integers.txt

--- sqaure_cube_integers/square_cube_integer.py
+++ sqaure_cube_integers/square_cube_integer.py
@@ -1,8 +1,14 @@
 # square all even integers and cube all odd integers
-
 # setup main class
+class SquareCube():
 ## setup main method
+    def __init__(self):
+### list to contain every number in the text file
+        self.numbers = []
 ### open file iterating every line on the file
+        with open("square_cube_integers/integers.txt", "r") as integers:
+            for line in integers:
+                self.numbers.append(line)
 ### seperate every number in the file if they're even or odd
 ### square every even number and cube every odd number
 ### input the perspective files into their rightful text file
